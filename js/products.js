33bb56c9228d1a79b08001cbd989c7772fe2bda3
buypal-store <yazidchahua10@hotmail.com>
2026-07-16 13:24:09 -0500

    Add 'Mini Bands' product to productosData
    Added a new product 'Mini Bands' to the productosData array.

--- js/products.js
+++ js/products.js
@@ -4,5 +4,6 @@ window.productosData = [
   { sku: "SPINNING-BIKE-DH68", nombre: "Spinning Bike DH68 8kg", precio: 359, imagen: "imagenes/Spinning bike DH68 8 kg.jpeg" },
   { sku: "xd", nombre: "Spinning Bike MZYS 10kg", precio: 459, imagen: "imagenes/Spinning bike MZY 10 kg.jpeg" },
   { sku: "MAQUINA-ABDOMINALES-22366", nombre: "Maquina Abdominales", precio: 169, imagen: "imagenes/Abdominales.jpeg" },
+  { sku: "MINI-BANDS", nombre: "Mini Bands", precio: 19, imagen: "imagenes/bandas elasticas.jpg" },
 
 ];
